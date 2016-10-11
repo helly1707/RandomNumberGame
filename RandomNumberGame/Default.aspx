1a404860097f019b0b30b20f5f0b87b9af53ea88
helly1707 <helly_kutchi@yahoo.com>
2016-10-11 11:13:20 -0400

    Add files via upload
    Lab 5 - Number Guessing Game

---
+++ RandomNumberGame/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RandomNumberGame._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+    <style>
+    html
+    {
+        background: url(Images/background.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="background-color:#19669C; margin:-10px 20%; height:1000px; color:#BFFDFC;">
+        <asp:Button ID="btnPlay" runat="server" Text="PlayNow" BackColor="#BFFDFC" 
+            style=" margin:5%; border-radius:10px;" Width="150px" Height="50px" 
+            Font-Size="XX-Large" onclick="btnPlay_Click" />
+        <div>
+            <asp:Panel ID="Panel1" runat="server" Visible = "false" BackColor="Transparent" 
+                BorderColor="#BFFDFC" BorderStyle="Dashed" style="border-radius:10px; margin:1% 5%; padding:2%;">
+
+                Random Number:
+                <asp:TextBox ID="txtRandom" runat="server" Text = "?" ReadOnly="true" style="background-color:Transparent; border-radius:10px; height:30px; width:50px; color:#BFFDFC; font-size:20px; font-weight:bold; text-align:center;"></asp:TextBox>
+
+                <br />
+                Choose Number between 1 to 100: 
+                <asp:TextBox ID="txtNumber" runat="server" style="background-color:Transparent; border-radius:10px; height:30px; width:50px; color:#BFFDFC; font-size:20px; font-weight:bold; text-align:center;"></asp:TextBox>
+
+                 <asp:Button ID="btnTry" runat="server" Text="Try Your Luck" BackColor="#BFFDFC" 
+                style=" margin:5%; border-radius:10px;" Width="150px" Height="30px" 
+                Font-Size="Large" OnClick="btnTry_Click" />
+                <br />
+                <asp:Label ID="lblMsg" runat="server" Text="" style="font-size:20px;"></asp:Label>
+
+            </asp:Panel>
+
+            <asp:Panel ID="Panel2" runat="server" Visible = "false" BackColor="Transparent" 
+                BorderColor="#BFFDFC" BorderStyle="Dashed" style="border-radius:10px; margin:1% 5%; padding:2%;">
+
+                You have a High Score:
+                <asp:TextBox ID="txtHighScore" runat="server" Text = "" ReadOnly="true" style="background-color:Transparent; border-radius:10px; height:30px; width:50px; color:#BFFDFC; font-size:20px; font-weight:bold; text-align:center;"></asp:TextBox>
+
+                <br />
+                Enter Your NickName: 
+                <asp:TextBox ID="txtNickName" runat="server" style="background-color:Transparent; border-radius:10px; height:30px; width:50px; color:#BFFDFC; font-size:20px; font-weight:bold; text-align:center;"></asp:TextBox>
+
+                 <asp:Button ID="btnSaveHighScore" runat="server" Text="Save High Score" BackColor="#BFFDFC" 
+                style=" margin:5%; border-radius:10px;" Width="150px" Height="30px" 
+                Font-Size="Large" OnClick="btnSaveHighScore_Click" />
+
+                <br />
+                <asp:Label ID="lblScore" runat="server" Text=""></asp:Label>
+            </asp:Panel>
+
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RandomNumberGame/Default.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace RandomNumberGame
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public static int Try = 100;
+        public static int[] GuessedNo = new int[100];
+        public static int Guess = 0;
+        public static NameValueCollection nvcScore = new NameValueCollection();
+        public static ArrayList arr = new ArrayList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void btnPlay_Click(object sender, EventArgs e)
+        {
+            Try = 100;
+            Guess = 0;
+            GuessedNo = new int[100];
+            txtHighScore.Text = "";
+            txtNickName.Text = "";
+            txtNumber.Text = "";
+            txtRandom.Text = "?";
+            lblMsg.Text = "";
+            lblScore.Text = "";
+
+            Session["RandomNo"] = "0"; ;
+            Panel1.Visible = true;
+            Panel2.Visible = false;
+            Random r = new Random();
+            Session["RandomNo"] = r.Next(1, 100);
+        }
+
+        protected void btnTry_Click(object sender, EventArgs e)
+        {
+            int RandomNo = Convert.ToInt32(Session["RandomNo"].ToString());
+            if (txtNumber.Text.ToString() != "")
+            {
+                int User_input = Convert.ToInt32(txtNumber.Text.ToString());
+                lblMsg.ForeColor = System.Drawing.Color.FromArgb(0, 191, 253, 252);
+
+                GuessedNo[Guess] = User_input;
+                Guess++;
+
+                if (Try > 0)
+                {
+                    if (User_input < RandomNo)
+                    {
+                        Try--;
+                        lblMsg.Text = "You have " + Try.ToString() + " left. The number you Guessed is Too Low";
+                    }
+                    else if(User_input > RandomNo)
+                    {
+                        Try--;
+                        lblMsg.Text = "You have " + Try.ToString() + " left. The number you Guessed is Too High";
+                    }
+                    else
+                    {
+                        txtRandom.Text = RandomNo.ToString();
+                        lblMsg.Text = "Congratulations!!! You Win the Game.";
+                        lblMsg.Text += "Your Score is: " + (100 - Guess);
+                        Panel2.Visible = true;
+                        txtHighScore.Text = (100 - Guess).ToString();
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "Oops!! No more Try Left. Play Again.";
+                    lblMsg.ForeColor = System.Drawing.Color.Red;
+                }
+                        
+            }
+            else
+            {
+                lblMsg.Text = "Please Enter your Guessed Number in TextBox.";
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        protected void btnSaveHighScore_Click(object sender, EventArgs e)
+        {
+            arr.Clear();
+            int i = 0;
+            if (txtNickName.Text != "")
+            {
+                nvcScore.Add(txtNickName.Text.ToString(), txtHighScore.Text.ToString());
+                foreach(string str in nvcScore)
+                {
+                    arr.Add(nvcScore[str].ToString());
+                }
+
+                //arr.Reverse();
+                foreach(string str in arr)
+                {
+                    foreach (string key in nvcScore.Keys)
+                    {
+                        string[] values = nvcScore.GetValues(key);
+                        foreach (string value in values)
+                        {
+                            if (value == str)
+                            {
+                                if (i <= 4)
+                                {
+                                    if (lblScore.Text == "")
+                                        lblScore.Text = key + "-" + str;
+                                    else
+                                        lblScore.Text += "<br />" + key + "-" + str;
+
+                                    i++;
+                                    break;
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                WriteTextFile();
+            }
+            else
+            {
+                lblMsg.Text = "Please Enter NickName to Save Score.";
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        public void WriteTextFile()
+        {
+            string FilePath = HttpContext.Current.Request.PhysicalApplicationPath +"\\Scores\\Score.txt";
+            System.IO.StreamWriter file = new System.IO.StreamWriter(FilePath);          
+            file.WriteLine(lblScore.Text.ToString());
+            file.Close();
+        }
+    }
+}
